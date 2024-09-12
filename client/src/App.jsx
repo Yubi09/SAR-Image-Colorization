b0e035ad4529d40854ba86bcc070d0e74d7e11dd
Arnab <arnabsantra248@gmail.com>
2024-09-12 20:50:59 +0530

    merged home page responsiveness

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,40 +22,40 @@ const App = () => {
 	const PrivateRoute = ({ element }) => {
 		return isAuthenticated ? element : <Navigate to="/login" />;
 	};
-	// const action = useNavigationType();
-	// const location = useLocation();
-	// const pathname = location.pathname;
+	const action = useNavigationType();
+	const location = useLocation();
+	const pathname = location.pathname;
 
-	// useEffect(() => {
-	// 	if (action !== "POP") {
-	// 		window.scrollTo(0, 0);
-	// 	}
-	// }, [action, pathname]);
+	useEffect(() => {
+		if (action !== "POP") {
+			window.scrollTo(0, 0);
+		}
+	}, [action, pathname]);
 
-	// useEffect(() => {
-	// 	let title = "";
-	// 	let metaDescription = "";
+	useEffect(() => {
+		let title = "";
+		let metaDescription = "";
 
-	// 	switch (pathname) {
-	// 		case "/":
-	// 			title = "";
-	// 			metaDescription = "";
-	// 			break;
-	// 	}
+		switch (pathname) {
+			case "/":
+				title = "";
+				metaDescription = "";
+				break;
+		}
 
-	// 	if (title) {
-	// 		document.title = title;
-	// 	}
+		if (title) {
+			document.title = title;
+		}
 
-	// 	if (metaDescription) {
-	// 		const metaDescriptionTag = document.querySelector(
-	// 			'head > meta[name="description"]'
-	// 		);
-	// 		if (metaDescriptionTag) {
-	// 			metaDescriptionTag.content = metaDescription;
-	// 		}
-	// 	}
-	// }, [pathname]);
+		if (metaDescription) {
+			const metaDescriptionTag = document.querySelector(
+				'head > meta[name="description"]'
+			);
+			if (metaDescriptionTag) {
+				metaDescriptionTag.content = metaDescription;
+			}
+		}
+	}, [pathname]);
 
 	return (
 		<div>
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-// import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals";
 import "./global.css";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</StrictMode>
 );
 
-// reportWebVitals();
+reportWebVitals();
